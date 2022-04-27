7741831ca8bc5d1104ef882a38ba36dabf60367b
Hanyecz Marcell <marcellhanyecz@gmail.com>
2022-04-27 21:49:29 +0200

    main repository created

---
+++ PizzaDelivery.Repository/IMainRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace PizzaDelivery.Repository
+{
+    public interface IMainRepository
+    {
+        IAddressRepository AddressRepo { get; set; }
+        ICourierRepository CourierRepo { get; set; }
+        ICustomerRepository CustomerRepo { get; set; }
+        IOrderRepository OrderRepo { get; set; }
+        IPizzaRepository PizzaRepo { get; set; }
+    }
+}
---
+++ PizzaDelivery.Repository/MainRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaDelivery.Repository
+{
+    public class MainRepository : IMainRepository
+    {
+        public IAddressRepository AddressRepo { get; set; }
+        public IPizzaRepository PizzaRepo { get; set; }
+        public ICustomerRepository CustomerRepo { get; set; }
+        public ICourierRepository CourierRepo { get; set; }
+        public IOrderRepository OrderRepo { get; set; }
+
+        public MainRepository(IAddressRepository addressRepo, IPizzaRepository pizzaRepo
+            , ICustomerRepository customerRepo, ICourierRepository courierRepo, IOrderRepository orderRepo)
+        {
+            AddressRepo = addressRepo;
+            PizzaRepo = pizzaRepo;
+            CustomerRepo = customerRepo;
+            CourierRepo = courierRepo;
+            OrderRepo = orderRepo;
+        }
+    }
+}
